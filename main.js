c86a8b13d76804b4867d4097152476e5c0b668a0
efim-sys <81066363+efim-sys@users.noreply.github.com>
2022-03-24 16:58:48 +0300

    add game

---
+++ main.js
@@ -0,0 +1,108 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 950;
+canvas.height = 950;
+
+let useNet = true;
+
+let colors = ["#26cc31", "#fed93c", "#ff8200", "#f55343", "#e71919"];
+
+const rect = canvas.getBoundingClientRect();
+
+let grid = new Array(10).fill(null).map(() => new Array(10).fill(null).map(() =>  new Array(4).fill(0)));
+
+resolution = 950/8;
+
+ctx.font = "100px Arial";
+
+function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for(let y = 0; y < 8; y++) {
+    for(let x = 0; x < 8; x++) {
+      const hint = grid[y][x][0];
+
+      ctx.beginPath();
+      ctx.fillStyle = colors[grid[y+1][x+1][1]];
+
+      if(!grid[y+1][x+1][0] && grid[y+1][x+1][2]) ctx.fillText((grid[y+1][x+1][1]).toString(), y*resolution+0.28*resolution, x*resolution+0.7*resolution);
+	  ctx.fillStyle = colors[4];
+	  if(grid[y+1][x+1][3] && !grid[y+1][x+1][2]) ctx.fillText("⚑", y*resolution+0.28*resolution, x*resolution+0.7*resolution);
+      if(useNet) ctx.strokeStyle = "#1a1a1a";
+      else ctx.strokeStyle = "black";
+      ctx.rect(y * resolution, x * resolution, resolution, resolution);
+      ctx.stroke();
+    }
+  }
+}
+
+async function generateHints() {
+  for(let y = 1; y < 9; y++) {
+    for(let x = 1; x < 9; x++) {
+      grid[x][y][0] = +(getRandomInt(6)==0);
+    }
+ }
+
+for(let y = 1; y < 9; y++) {
+   for(let x = 1; x < 9; x++) {
+     for (let i = -1; i < 2; i++) {
+       for (let j = -1; j < 2; j++) {
+         if (i === 0 && j === 0) {
+           continue;
+         }
+         if (grid[x+i][y+j][0] == 1) {
+            grid[x][y][1] += 1;
+         }
+        }
+      }
+    }
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+canvas.addEventListener("click", (e) => {
+  var gridX = Math.floor((e.clientX - rect.left)/resolution);
+  var gridY = Math.floor((e.clientY - rect.top)/resolution);
+  grid[gridX+1][gridY+1][2] = 1;
+  if(grid[gridX+1][gridY+1][0]) {
+	alert("game over!");
+	location.reload();
+  }
+  console.log(gridY);
+  console.log(gridX);
+  render();
+  checkWin();
+});
+
+canvas.addEventListener("contextmenu", (e) => {
+  var gridX = Math.floor((e.clientX - rect.left)/resolution);
+  var gridY = Math.floor((e.clientY - rect.top)/resolution+0.3);
+  grid[gridX+1][gridY+1][3] = !grid[gridX+1][gridY+1][3];
+  console.log(gridY);
+  console.log(gridX);
+  render();
+  checkWin();
+});
+
+function checkWin() {
+	let a = 0;
+	for(let y = 1; y < 9; y++) {
+		for(let x = 1; x < 9; x++) {
+			if(!(grid[y][x][3] || grid[y][x][2])) {
+				a += 1;
+				
+			} 
+			
+		}
+	}
+	console.log(a);
+	if(a==0){
+		alert("You Win!");
+	}
+}
+
+generateHints();
+
+render();
